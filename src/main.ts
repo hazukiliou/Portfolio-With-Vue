97e3eacf92bb65163c850d3c4dea4f05f57b0a7e
hazuki <hazuki.liou@gmail.com>
2023-08-01 02:52:38 +0800

    modify: google analytics  => gtag

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import VueGtag from 'vue-gtag'
+import VueGtagConfig from '@/plugin/VueGtagConfig'
 
 const pinia = createPinia()
 
-createApp(App).use(router).use(pinia).mount('#app')
+createApp(App)
+  .use(router)
+  .use(pinia)
+  .use(VueGtag, VueGtagConfig)
+  .mount('#app')
---
+++ src/plugin/VueGtagConfig.ts
@@ -0,0 +1,6 @@
+const config = {
+  disableScriptLoad: process.env.NODE_ENV === 'development',
+  config: { id: 'G-XEZDHQZ5JD' }
+}
+
+export default config
